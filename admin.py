afc9f16b49cf6b3475cececaa9f8bc1d8682470d
Cristian <linux@DESKTOP-RIGNI95.localdomain>
2022-10-31 20:52:23 -0500

    eliminando archivos basura

--- admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Candidato,Persona,Cargo,Lista,Urna,Periodo
-# Register your models here.
-admin.site.register([Candidato,Persona,Cargo,Lista,Urna,Periodo])
-#,Lista,Urna,Periodo])
